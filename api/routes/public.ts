83c2489d83962c6da4021a33c1701e36c78ff6d5
coresoft021 <38849384+coresoft021@users.noreply.github.com>
2018-10-09 22:46:19 +0530

    Update public.ts

--- api/routes/public.ts
+++ api/routes/public.ts
@@ -8,19 +8,7 @@ const Op = Sequelize.Op;
         
  
 
-publicRouter.get("/all", (request: Request, response: Response) => {
-  
 
-       Tas_test.sync({force: true}).then(() => {
-   //Table created
-  return Tas_test.create({
-    
-     ITEM_NAME : 'a',
-   
-    
-      
-    
-    }) })
 
 
 publicRouter.get("/about", (request: Request, response: Response) => {
@@ -33,44 +21,9 @@ publicRouter.get("/about", (request: Request, response: Response) => {
 
 });
 
-publicRouter.get("/cdb", (request: Request, response: Response) => {
-   Tas_test.sync({force: true}).then(() => {
-   //Table created
-  return Tas_test.create({
-    
-   
-     ITEM_NAME: 'papi',
-     ITEM_COUNT: 0,
-      ON_STOCK : 0,
-    
-    
-    
-    })
-   })
 
 
-  publicRouter.get('/test_all', (request: Request, response: Response) => {
- 
-   
-      Pawn_customers.findAll({
-        
-                                })
-         .then(users=> 
-              {
-                 if(users)
-                    {
-                       response.send(users);
 
-                    }
-
-               else
-                   {
-                       return response.json({success:true, msg:'No records found'});
-                   }
-        
-             })
-   
-   })
 
 publicRouter.get("/server", (request: Request, response: Response) => {
   
