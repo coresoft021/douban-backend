336dc58bf996e9d32b8aa8fc1b926dd15cc184c9
coresoft021 <38849384+coresoft021@users.noreply.github.com>
2018-10-19 21:46:13 +0530

    Update app.ts

--- api/app.ts
+++ api/app.ts
@@ -2,12 +2,20 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+import { json, urlencoded } from "body-parser";
+import { Request, Response, Router } from "express";
+import { publicRouter } from "./routes/public";
+
 const port =  process.env.PORT || 8080;
 
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/public'));
+app.use(cors());
+app.use(json());
+app.use(urlencoded({ extended: true }));
 
+app.use(express.static(__dirname + '/public'));
+app.use("/api/public", publicRouter);
 app.get('/api', function (req, res) {
     res.send('Hello World, How are you!');
 });
