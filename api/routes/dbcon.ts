7d5093a598d6d76dddee3c0a6c4af48584be8642
coresoft021 <38849384+coresoft021@users.noreply.github.com>
2018-10-19 15:08:46 +0530

    Delete dbcon.ts

--- api/routes/dbcon.ts
+++
@@ -1,7 +0,0 @@
-import { Sequelize } from "sequelize";
-
-
-var sequelize = new Sequelize('postgres://eabenglqnnlxii:3895dc322b94f951b9e3eddc203744d5fcfa9c228c504b489cd1a96f875c74cd@ec2-54-235-193-34.compute-1.amazonaws.com:5432/d50eu6rmqqbp5r');
-
-
-export {   Sequelize, sequelize };
