983ac50895bfe7d8e0eb5b7d7373abd0c2c9e154
coresoft021 <38849384+coresoft021@users.noreply.github.com>
2018-10-19 22:44:04 +0530

    Update public.ts

--- api/routes/public.ts
+++ api/routes/public.ts
@@ -1,10 +1,23 @@
 import { Request, Response, Router } from "express";
+import { Sequelize, sequelize } from './dbcon';
+import { Tas_users } from '../models/tas_users';
 
+const Op = Sequelize.Op;
 const publicRouter: Router = Router();
 
               
                
 
+dbRouter.get("/cdb", (request: Request, response: Response) => {
+  Tas_users.sync({force: true}).then(() => {
+  // Table created
+  return Tas_users.create({
+    firstName: 'John',
+    lastName: 'Hancock'
+  });
+});
+
+  })
 
 
 publicRouter.get("/about", (request: Request, response: Response) => {
