457e068ba912635401b2ac458aad6cde5ded5673
coresoft021 <38849384+coresoft021@users.noreply.github.com>
2019-04-09 23:02:21 +0530

    Create dbcon.js

---
+++ dist/server/routes/dbcon.js
@@ -0,0 +1,6 @@
+"use strict";
+
+const sequelize_1 = require("sequelize");
+exports.Sequelize = sequelize_1.Sequelize;
+var sequelize = new sequelize_1.Sequelize('postgres://xzqyirvy:Vlw2zQL-SVj5fTCDkskOguzkWEHn7Ea-@balarama.db.elephantsql.com:5432/xzqyirvy');
+exports.sequelize = sequelize;
